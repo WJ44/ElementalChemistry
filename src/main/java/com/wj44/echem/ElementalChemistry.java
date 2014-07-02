e79d0576e8e70aaf21b7b4523370e090982d5e1b
WJ44 <wesleyjoosten@hotmail.nl>
2014-07-02 19:49:07 +0200

    Added Configuration UI(not working (yet))

--- src/main/java/com/wj44/echem/ElementalChemistry.java
+++ src/main/java/com/wj44/echem/ElementalChemistry.java
@@ -1,6 +1,6 @@
 package com.wj44.echem;
 
-import com.wj44.echem.configuration.ConfigurationHandler;
+import com.wj44.echem.handler.ConfigurationHandler;
 import com.wj44.echem.proxy.IProxy;
 import com.wj44.echem.reference.Reference;
 import cpw.mods.fml.common.Mod;
@@ -10,9 +10,13 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 /**
- * Created by Wesley on 22-6-2014.
+ * Created by Wesley "WJ44" Joosten on 22-6-2014.
+ *
+ * Part of the ElementalChemistry Mod, distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
  */
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class ElementalChemistry
 {
     @Mod.Instance(Reference.MOD_ID)
