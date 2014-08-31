d872e655615074ae31779bdee7c01ebc18502e52
WJ44 <wesleyjoosten@hotmail.nl>
2014-08-31 13:56:06 +0200

    Added keybindings setup

---
+++ src/main/java/com/wj44/echem/reference/Names.java
@@ -0,0 +1,18 @@
+package com.wj44.echem.reference;
+
+/**
+ * Created by Wesley "WJ44" Joosten on 31-8-2014.
+ *
+ * Part of the ElementalChemistry Mod, distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (https://creativecommons.org/licenses/by-nc-sa/3.0/)
+ */
+public final class Names
+{
+    public static final class Keys
+    {
+        public static final String CATEGORY = "keys.echem.category";
+        public static final String GUI = "keys.echem.gui";
+
+    }
+}
